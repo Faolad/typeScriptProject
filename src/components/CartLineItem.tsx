f153ac70d4cdb3ecc8b451061c877f12adfca0e1
Faolad <adeyeyea20@gmail.com>
2023-10-03 23:42:17 +0100

    nc

--- src/components/CartLineItem.tsx
+++ src/components/CartLineItem.tsx
@@ -1,9 +1,48 @@
+import {ReactElement, changeEvent} from 'react'
+import { CartItemType } from "../context/CartProvider"
+import { ReducerAction, ReducerActionType  } from "../context/CartProvider"
 
 
-const CartLineItem = () => {
-  return (
-    <div>CartLineItem</div>
-  )
+type PropsType = {
+    item: CartItemType,
+    dispatch: React.Dispatch<ReducerAction>,
+    REDUCER_ACTIONS: ReducerActionType,
+
 }
+const CartLineItem = ({item, dispatch, REDUCER_ACTIONS}: PropsType) => {
+ 
+    const img: string = new URL(`../images/${item.sku}.jpg`, import.meta.url).href
+    
+    const lineTotal:number = (item.qty * item.price )
+    const highestQty: number = 20 > item.qty ? 20 : item.qty
+
+    const optionValues: number[] = [...Array(highestQty).keys()].map(i => i+1) 
+
+    const options:ReactElement[] = optionValues.map(val => {
+        return <option key={`opt${val}`} value={val}>{val}</option>
+    })
+
+    const onChangeQty = (e: changeEvent<HTMLSelectElement>)=>{
+        dispatch({
+            type: REDUCER_ACTIONS.QUANTITY,
+            payload: {...item, qty:Number(e.target.value)}
+        })
+    }
+
+    const onRemoveFromCart = ()=> dispatch({
+        type:REDUCER_ACTIONS.REMOVE,
+        payload: item
+        // payload: {...item}
+    })
+    
+    const content = (
+        <li className="cart__item">
+            <img src="{img}" alt="{item.name}" className="cart__img" />      
+        </li>
+    )
+
+    return content
+
+    }
 
 export default CartLineItem
